17403abf7a1f13e3ca93dd259670356126d3637a
mtndl96 <metindal96@gmail.com>
2018-12-27 01:35:03 +0300

    Çarpma butonunun işlemi yapması sağlandı.

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -149,6 +149,7 @@
             this.button4.TabIndex = 11;
             this.button4.Text = "Çarpma";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -158,6 +159,7 @@
             this.button5.TabIndex = 12;
             this.button5.Text = "Çıkış";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // label7
             // 
